a16c3a8146f10e62defb8985acbc6fbe022b257b
MaxShulha94 <maxshulga94@gmail.com>
2023-11-26 18:10:35 +0200

    hw_8

---
+++ currency/currency/forms.py
@@ -0,0 +1,33 @@
+from django import forms
+
+from .models import Rate, ContactUs, Source
+
+
+class RateForm(forms.ModelForm):
+    class Meta:
+        model = Rate
+        fields = (
+            'buy',
+            'sell',
+            'type',
+            'source',
+        )
+
+
+class ContactUsForm(forms.ModelForm):
+    class Meta:
+        model = ContactUs
+        fields = (
+            'email_from',
+            'subject',
+            'message',
+        )
+
+
+class SourceForm(forms.ModelForm):
+    class Meta:
+        model = Source
+        fields = (
+            'source_url',
+            'name'
+        )
